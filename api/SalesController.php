2a010182466545dc73214968501c1c5af62a19c4
Eric Anicet <anicetkeric@gmail.com>
2017-12-17 22:41:34 +0000

    add sales class

---
+++ api/SalesController.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ANICET ERIC KOUAME
+ * Date: 09/12/2017
+ * Time: 04:33
+ */
+
+require_once  $_SERVER['DOCUMENT_ROOT'].'/odooApiXmlRpc/helpers/ripcord.php';
+require_once  $_SERVER['DOCUMENT_ROOT'].'/odooApiXmlRpc/manager/SalesManager.php';
+require_once  $_SERVER['DOCUMENT_ROOT'].'/odooApiXmlRpc/domain/Sales.php';
+
+
+
+$METHOD='';
+//Action
+if(@isset($_REQUEST['action'])) $METHOD=htmlentities($_REQUEST['action']);
+
+//Initialise variables
+$name = null;
+$state= null;
+$date_order= null;
+ $s_id= null;
+
+
+
+//get all post variable
+$name =htmlentities(@$_REQUEST['name']);
+$state =htmlentities(@$_REQUEST['state']);
+$date_order =htmlentities(@$_REQUEST['date_order']);
+$s_id =htmlentities(@$_REQUEST['user_id']);
+
+//build objects
+$s = new SalesManager();
+$sales = new Sales();
+
+$sales->setName($name);
+$sales->setState($state);
+$sales->setDateOrder($date_order);
+$sales->setUserId($s_id);
+
+
+switch($METHOD){
+
+
+    case 'all':
+    //http://localhost:1180/odooApiXmlRpc/api/SalesController.php?action=all
+        $s->getAllSalesOrders($name);
+        break;
+
+    default:
+
+        echo 'Error';
+        break;
+
+}
+
--- helpers/config.php
+++ helpers/config.php
@@ -1,9 +1,9 @@
 <?php
 
 //put here your server informations
-define("URL", "http://localhost:8069"); //write your odoo url link
-define("DB", "demo"); //write your database name
-define("DB_USER", "demo@outlook.com"); //write your db username
+define("URL", "http://vps456932.ovh.net:8069"); //write your odoo url link
+define("DB", "Basic_demo"); //write your database name
+define("DB_USER", "anicetkeric@outlook.com"); //write your db username
 define("DB_PASSWORD", "azerty@"); //write your db password
 
 ?>
