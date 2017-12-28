bf66f278dfcd9ea10dedab7dcbf57f8162f00585
AEK <anicetkeric@outlook.com>
2017-12-28 22:13:43 +0000

    Update config.php

--- helpers/config.php
+++ helpers/config.php
@@ -1,9 +1,9 @@
 <?php
 
 //put here your server informations
-define("URL", "http://vps456932.ovh.net:8069"); //write your odoo url link
-define("DB", "Basic_demo"); //write your database name
-define("DB_USER", "anicetkeric@outlook.com"); //write your db username
-define("DB_PASSWORD", "azerty@"); //write your db password
+define("URL", "http://localhost:8069"); //write your odoo url link
+define("DB", "DB_Demo"); //write your database name
+define("DB_USER", "your_email"); //write your db username
+define("DB_PASSWORD", "password"); //write your db password
 
 ?>
