a44f4cb93f3a2a5dbcfdd75848920278f98a3974
Eric Anicet <anicetkeric@gmail.com>
2018-01-13 20:16:44 +0000

    small change

--- helpers/config.php
+++ helpers/config.php
@@ -1,8 +1,8 @@
 <?php
 
 //put here your server informations
-define("URL", "http://vps456932.ovh.net:8069"); //write your odoo url link
-define("DB", "Basic_demo"); //write your database name
+define("URL", "http://localhost:8069"); //write your odoo url link
+define("DB", "demo"); //write your database name
 define("DB_USER", "anicetkeric@outlook.com"); //write your db username
 define("DB_PASSWORD", "azerty@"); //write your db password
 
